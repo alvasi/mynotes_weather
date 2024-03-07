9baf43b7ba1f96ebe9bcdf93e90baf8fd926895c
Alva Si <alva.si19@imperial.ac.uk>
2024-03-07 21:08:15 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import requests
+import os
+
+app = Flask(__name__)
+
+API_KEY = os.environ.get("API_KEY")
+BASE_URL = "http://api.weatherstack.com/"
+
+
+@app.route("/current_weather", methods=["GET"])
+def current_weather():
+    city = request.args.get("city")
+    response = requests.get(f"{BASE_URL}current?access_key={API_KEY}&query={city}")
+    if not response.ok:
+        return None, "Error fetching weather data"
+    weather_data = response.json()
+    temperature = weather_data["current"]["temperature"]
+    weather_description = (
+        weather_data["current"]["weather_descriptions"][0]
+        if weather_data["current"]["weather_descriptions"]
+        else "Description not available"
+    )
+    return jsonify(temperature=temperature, weather_description=weather_description)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
